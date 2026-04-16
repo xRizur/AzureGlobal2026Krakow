c9c55dfc8a57342e902306a679188db90d712e80
xrizur <xrizur@o2.pl>
2026-04-16 21:07:22 +0200

    add database connectionstring

--- main.tf
+++ main.tf
@@ -71,7 +71,7 @@ module "app_service" {
   }
   app_settings = {
     "ApplicationInsights__ConnectionString" = module.application_insights.connection_string
-    "ConnectionStrings__RazorPagesMovieContext" = "string"
+    "ConnectionStrings__RazorPagesMovieContext" = "Server=tcp:mssqluser15.database.windows.net,1433;Initial Catalog=db-user15;Persist Security Info=False;User ID=user15;Password=Eh(HoUk:r$B(%=1gV!Z-nV:z+U?W@6K5tM5};MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;"
     "AzureKeyVault__SecretKey" = "string"
   }
 }
