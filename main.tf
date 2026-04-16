e585d84317952565e65a7aa59de8330189b35bd4
xrizur <xrizur@o2.pl>
2026-04-16 19:43:06 +0200

    0.1 check

--- main.tf
+++ main.tf
@@ -10,6 +10,16 @@ provider "azurerm" {
   features {}
 }
 
+locals {
+    resource_group_name = "rg-user15"
+    location            = "westeurope"
+}
+
+data "azurerm_user_assigned_identity" "example" {
+  name                = "ga-mi-user15"
+  resource_group_name = local.resource_group_name
+}
+
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-user15" #change here
@@ -17,4 +27,71 @@ terraform {
     container_name       = "tfstate"
     key                  = "terraform.tfstate"
   }
+}
+
+module "keyvault" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=keyvault/v1.0.0"
+  # also any inputs for the module (see below)
+
+  keyvault_name = "kv-user15"
+  resource_group = {
+    location = local.location
+    name     = local.resource_group_name
+  }
+  network_acls = {
+    default_action = "Allow"
+    bypass         = "AzureServices"
+    ip_rules       = ["0.0.0.0/0"]
+  }
+}
+
+module "service_plan" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=service_plan/v2.0.0"
+  # also any inputs for the module (see below)
+  app_service_plan_name = "asp-user15"
+  sku_name = "P1v2"
+  tags = {
+  }
+  resource_group = {
+    location = local.location
+    name     = local.resource_group_name
+  }
+}
+
+module "app_service" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=app_service/v1.0.0"
+  # also any inputs for the module (see below)
+  app_service_name = "as-user15"
+  app_service_plan_id = module.service_plan.app_service_plan_id
+  identity_client_id = module.keyvault.identity_client_id
+  identity_id = data.azurerm_user_assigned_identity.example.id
+  resource_group = {
+    location = local.location
+    name     = local.resource_group_name
+  }
+  app_settings = {}
+}
+
+module "mssql_server" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=mssql_server/v1.0.0"
+  # also any inputs for the module (see below)
+  resource_group = {
+    location = local.location
+    name     = local.resource_group_name
+  }
+  sql_server_admin = "user15"
+  sql_server_name = "mssql-user15"
+  sql_server_version = "12.0"
+}
+
+module "container_registry" {
+  source = "git::https://github.com/pchylak/global_azure_2026_ccoe.git?ref=container_registry/v1.0.0"
+  # also any inputs for the module (see below)
+    resource_group = {
+        location = local.location
+        name     = local.resource_group_name
+    }
+    container_registry_name = "acruser15"
+    read_access = ["${data.azurerm_user_assigned_identity.example.id}"]
+    write_access = ["${data.azurerm_user_assigned_identity.example.id}"]
 }
